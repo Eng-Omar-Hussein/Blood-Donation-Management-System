cfaf9808fe712862079b3a092f4615b934f28b4e
M0hamed-0mar <119747408+M0hamed-0mar@users.noreply.github.com>
2022-12-03 18:42:31 +0200

    Update main.cpp
    a new way to accept password from user

--- main.cpp
+++ main.cpp
@@ -5,37 +5,37 @@ class password {
 private:
 	string x, y = "123456789";
 public:
-	void set_password(string p) {
-		x = p;
-	}
-	bool check_password() {
+	void set_password(string p) { x = p;}
+	bool check_password() 
+	{
 		if (x == y) return false;
 		return true;
 	}
 };
-int main() {
+int main() 
+{
 	password owner;
 	for (int counter = 1;owner.check_password()== true;counter++)
         {
         string my_password;
 		if (counter > 1)
-        {
-			cout << "your password is wrong please enter a correct password or to quit the program enter < 0 > : ";
-			cin >> my_password;
-            owner.set_password(my_password);
+        	{
+		cout << "your password is wrong please enter a correct password or to quit the program enter < 0 > : ";
+		cin >> my_password;
+                owner.set_password(my_password);
 			if (my_password == "0")
 				counter=0;
 			if (counter == 0)
-            {
+            		{
 				cout << "see you soon\n";
 				break;
 			}
 		}
 		if (counter == 1)
-        {
-        cout << "please enter a correct password:\n";
+        	{
+        	cout << "please enter a correct password:\n";
 		cin >> my_password;
 		owner.set_password(my_password);
-        }
-	    }
+        	}
+	}
 }
