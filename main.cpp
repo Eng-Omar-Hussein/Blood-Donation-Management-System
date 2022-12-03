9b694d0758eb0c7111148846812f47c2e2f1b5b7
Omar Hussein <117474007+Eng-Omar-Hussein@users.noreply.github.com>
2022-12-03 16:23:12 +0200

    Register
    Check if user enter a correct password or not
    to sign in a system

--- main.cpp
+++ main.cpp
@@ -1,4 +1,34 @@
 #include<iostream>
+#include<string>
 using namespace std;
-int main(){
+class password {
+private:
+	string x, y = "123456789";
+public:
+	void set_password(string p) {
+		x = p;
+	}
+	bool check_password() {
+		if (x == y) return false;
+		return true;
+	}
+};
+int main() {
+	int counter = 0;
+	password owner;
+	string my_password;
+	do {
+		if (counter > 0) {
+			cout << "your password is wrong to submit another one, please enter the number < 0 >: ";
+			cin >> counter;
+			if (counter != 0) {
+				cout << "see you soon\n";
+				return 0;
+			}
+		}
+		cout << "please enter a correct password:\n";
+		cin >> my_password;
+		owner.set_password(my_password);
+		counter++;
+	} while (owner.check_password());
 }
